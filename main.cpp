1d7ab638b59704a1fd0a0424a56ac979e6fe6d99
root <root@MH320-78120.avc.edu>
2026-04-01 12:55:20 -0700

    a59 Done.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,15 @@ using namespace std;
 int main()
 {
 	int prev, current;
-	// TODO
-
-	// TODO
+	int flag = 1;
+	cout << "Insert current value\n";
+	cin >> current;
+	while ((flag == 1) or (prev > current)){
+		flag = 0;
+		cout << current << " " << endl;
+		prev = current;
+		cout << "Next input: \n";
+		cin >> current;
+	}
+	cout << "Input larger than previous value, program stopped." << endl;
 }
